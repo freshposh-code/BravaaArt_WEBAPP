a20735d5eba7d62b4895395f3c6a4ce259418d01
freshposh-code <atomisefarouk919@gmail.com>
2024-09-20 15:11:44 +0100

    "Refactored Collections and HeroSection components: updated imports, formatting, and layout."

--- components/Collections/Collections.tsx
+++ components/Collections/Collections.tsx
@@ -1,53 +1,53 @@
 'use client'
 
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 import { collectionX1, collectionX2 } from '@/Constant/Data'
 import TextShowCase from '../../Utils/Motions/TextShowCase'
-import { useScroll, useTransform , motion} from 'framer-motion';
+import { useScroll, useTransform, motion } from 'framer-motion';
 import NftCards from './NftCards';
 
 const Collections = () => {
     const container = useRef(null);
-const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ["start end", "end start"]
-})
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ["start end", "end start"]
+    })
 
-const x1 = useTransform(scrollYProgress, [0, 1], [0, 150])
-const x2 = useTransform(scrollYProgress, [0, 1], [0, -150])
-  return (
-    <section ref={container} id='discover' className='collection__gradient'>
-        <div className="flex items-center justify-center mt-[18rem] m-6 pt-6">
-        <TextShowCase fontSize='text-white sm:text-[2.2rem] xm:text-[2rem] text-[1.7rem] font-bold'> <span className='sm:text-[2.4rem] text-[2rem] font-serif'>NFT</span> Collections</TextShowCase>
-        </div>
+    const x1 = useTransform(scrollYProgress, [0, 1], [0, 150])
+    const x2 = useTransform(scrollYProgress, [0, 1], [0, -150])
+    return (
+        <section ref={container} id='discover' className='collection__gradient'>
+            <div className="flex items-center justify-center sm:mt-[18rem] mt-[10rem] m-6 pt-6">
+                <TextShowCase fontSize='text-white sm:text-[2.2rem] xm:text-[2rem] text-[1.7rem] font-bold'> <span className='sm:text-[2.4rem] text-[2rem] font-serif'>NFT</span> Collections</TextShowCase>
+            </div>
 
-          <div className="flex justify-center items-center m-6 text-xs">
-        <TextShowCase fontSize='text-slate-300 sm:text-base text-[0.9em]'>Top collections over this last year you should look for</TextShowCase>
-        </div>
+            <div className="flex justify-center items-center m-6 text-xs">
+                <TextShowCase fontSize='text-slate-300 sm:text-base text-[0.9em]'>Top collections over this last year you should look for</TextShowCase>
+            </div>
 
-        <div className="md:flex hidden flex-col overflow-x-hidden">
+            <div className="md:flex hidden flex-col overflow-x-hidden">
 
-        <motion.div className="text-white flex gap-4 slider" style={{x: x1}}>
-            {collectionX1.map((item) => (
-                <div key={item.Desc} className='flex justify-center items-center'>
-                       <h2 className='filter-dark p-5 rounded-[33px] font-semibold'>{item.Desc}</h2>
-                </div>
-            ))}
-        </motion.div>
+                <motion.div className="text-white flex gap-4 slider" style={{ x: x1 }}>
+                    {collectionX1.map((item) => (
+                        <div key={item.Desc} className='flex justify-center items-center'>
+                            <h2 className='filter-dark p-5 rounded-[33px] font-semibold'>{item.Desc}</h2>
+                        </div>
+                    ))}
+                </motion.div>
 
 
-        <motion.div className="text-white flex mt-3 slider" style={{x: x2}}>
-            {collectionX2.map((item) => (
-                <div key={item.Desc} className='flex justify-center items-center'>
-                       <h2 className='filter-dark p-5 rounded-[33px] font-semibold'>{item.Desc}</h2>
-                </div>
-            ))}
-        </motion.div>
-        </div>
-       
-       <NftCards />
-    </section>
-  )
+                <motion.div className="text-white flex mt-3 slider" style={{ x: x2 }}>
+                    {collectionX2.map((item) => (
+                        <div key={item.Desc} className='flex justify-center items-center'>
+                            <h2 className='filter-dark p-5 rounded-[33px] font-semibold'>{item.Desc}</h2>
+                        </div>
+                    ))}
+                </motion.div>
+            </div>
+
+            <NftCards />
+        </section>
+    )
 }
 
 export default Collections 
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,41 +1,48 @@
 'use client'
 
-import SectionWrapper from '@/HOC/SectionWrapper' 
+import SectionWrapper from '@/HOC/SectionWrapper'
 import { motion } from 'framer-motion'
 // import Image from 'next/image'
 import AssistantDirectionIcon from '@mui/icons-material/AssistantDirection';
 import { feedbacks } from '@/Constant/Data';
 import TextShowCase from '../Utils/Motions/TextShowCase'
 import { fadeIn, zoomIn } from '@/Utils/Motions/motion';
+import Image from 'next/image';
 
 const HeroSection = () => {
 
   return (
-    <motion.section className='sm:px-20 px-6 sm:my-[10rem] my-28 flex sm:flex-row flex-col'>
+    <section className='flex sm:flex-row flex-col'>
+      <motion.section className='sm:px-20 px-6 sm:my-[10rem] my-28'>
         <div>
-        <motion.h1 variants={fadeIn('up', 'tween', 0.45, 0.58)} className='text-white capitalize sm:text-[4rem] xm:text-[1.8rem] text-[20px] font-extrabold  z-10 sm:leading-[5.3rem] leading-[2.8rem]'>
-            discover, collect, and <br /> sell extraordinary <br /> digital art <motion.div variants={fadeIn('right', 'tween', 0.80, 1.2)} className='curved-line sm:flex hidden'/></motion.h1>
-            
-             <motion.video variants={zoomIn(0.99, 0.99)} autoPlay muted loop width="650" height="320" className="absolute sm:top-[170px] top-[80px] left-0 z-[-2000]">
-              <source src='/blackhole.webm' type="video/webm" />
-             </motion.video>
+          <motion.h1 variants={fadeIn('up', 'tween', 0.45, 0.58)} className='text-white capitalize sm:text-[4rem] xm:text-[1.8rem] text-[20px] font-extrabold  z-10 sm:leading-[5.3rem] leading-[2.8rem]'>
+            discover, collect, and <br /> sell extraordinary <br /> digital art <motion.div variants={fadeIn('right', 'tween', 0.80, 1.2)} className='curved-line sm:flex hidden' /></motion.h1>
 
-            <motion.button variants={fadeIn('down', 'tween', 0.55, 0.88)} className='text-white mt-14 gradient sm:px-10 px-5 sm:py-5 py-3 rounded-[35px] font-semibold gap-1 items-center flex '>
-               <AssistantDirectionIcon style={{color: 'white'}}/> HIRE ME</motion.button>
-                
-                <div className='mt-14 flex sm:gap-20 gap-14 items-center flex-wrap'>
-                  {feedbacks.map((item, index) => (
-                 <div key={item.Desc}>
-               <motion.div variants={fadeIn('down', 'tween', 0.65, index * 0.75)} className={`border ${ index ? "border-violet-600" :  "border-green-600"} mb-3`} />
-               <motion.p variants={fadeIn('down', 'tween', index * 0.60, 0.45)} style={{color: item.textColor, fontWeight: 700}}>{item.Desc}</motion.p >
-               <TextShowCase fontSize="text-white sm:text-[2rem] xm:text-[1.5rem] text-[1rem] font-bold mt-3">{item.Num}</TextShowCase>
-                      </div>
-                  ))}
-                </div>
-            </div>
-            <div className="gradient-03 overflow-x-hidden" />
-      
-    </motion.section>
+          <motion.video variants={zoomIn(0.99, 0.99)} autoPlay muted loop width="650" height="320" className="absolute sm:top-[170px] top-[80px] left-0 z-[-2000]">
+            <source src='/blackhole.webm' type="video/webm" />
+          </motion.video>
+
+          <motion.button variants={fadeIn('down', 'tween', 0.55, 0.88)} className='text-white mt-14 gradient sm:px-10 px-5 sm:py-5 py-3 rounded-[35px] font-semibold gap-1 items-center flex '>
+            <AssistantDirectionIcon style={{ color: 'white' }} /> HIRE ME</motion.button>
+
+          <div className='mt-14 flex sm:gap-20 gap-14 items-center flex-wrap'>
+            {feedbacks.map((item, index) => (
+              <div key={item.Desc}>
+                <motion.div variants={fadeIn('down', 'tween', 0.65, index * 0.75)} className={`border ${index ? "border-violet-600" : "border-green-600"} mb-3`} />
+                <motion.p variants={fadeIn('down', 'tween', index * 0.60, 0.45)} style={{ color: item.textColor, fontWeight: 700 }}>{item.Desc}</motion.p >
+                <TextShowCase fontSize="text-white sm:text-[2rem] xm:text-[1.5rem] text-[1rem] font-bold mt-3">{item.Num}</TextShowCase>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="gradient-03 overflow-x-hidden" />
+
+
+      </motion.section>
+      <div>
+        <Image src="/Bravaaa.png" alt='bravvaa' width={1000} height={1000} className='min-w-full object-cover' />
+      </div>
+    </section>
   )
 }
 
